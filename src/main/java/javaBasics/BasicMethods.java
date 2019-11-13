9725e91af1f102881a05f8e025cccd18bad8e1a7
daryal89 <34196597+daryal89@users.noreply.github.com>
2019-11-13 17:30:37 -0500

    Add files via upload

---
+++ src/main/java/javaBasics/BasicMethods.java
@@ -0,0 +1,11 @@
+package javaBasics;
+
+public class BasicMethods {
+
+	public static void main(String[] args) {
+		System.out.println("This is my first Java course");
+	
+
+	}
+
+}
---
+++ src/main/java/javaBasics/BasicString.java
@@ -0,0 +1,12 @@
+package javaBasics;
+
+public class BasicString {
+	public static String str = "Hello Neapl!!!";
+
+	public static void main(String[] args) {
+		System.out.println("This is my first Java course");
+		System.out.println("The string is: " + str);
+
+	}
+
+}
